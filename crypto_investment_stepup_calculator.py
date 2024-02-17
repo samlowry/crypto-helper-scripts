340c335982a4b36574878056bae7abe6d2754a08
Samuel Lowry <samlowry@e-baka.net>
2024-02-17 20:12:11 +0700

    fixed pep8 errors

--- crypto_investment_stepup_calculator.py
+++ crypto_investment_stepup_calculator.py
@@ -1,12 +1,10 @@
-import sys
 import argparse
 import datetime
-from dateutil.relativedelta import relativedelta
 import requests
-import json
 import time
 
 DEFAULT_API_REQUEST_DELAY = 4  # Delay in seconds between each API request
+
 
 def get_crypto_ids(delay):
     response = requests.get("https://api.coingecko.com/api/v3/coins/list")
@@ -18,6 +16,7 @@ def get_crypto_ids(delay):
     else:
         print("Failed to retrieve cryptocurrency list.")
         return {}
+
 
 def get_current_price(crypto_id, delay):
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd"
@@ -33,16 +32,18 @@ def get_current_price(crypto_id, delay):
         print(f"Failed to retrieve current price for {crypto_id}.")
     return None
 
+
 def calculate_investments(start_date, end_date=None, step_percentage=1, amount=1, ticker='BTC', ticker_id=None, delay=DEFAULT_API_REQUEST_DELAY):
     if not end_date:
         end_date = datetime.datetime.now().strftime('%d-%m-%Y')
 
-    start_date = datetime.datetime.strptime(start_date, '%d-%m-%Y')  # Update date format to dd-mm-yyyy
-    end_date = datetime.datetime.strptime(end_date, '%d-%m-%Y')  # Update date format to dd-mm-yyyy
+    start_date = datetime.datetime.strptime(
+        start_date, '%d-%m-%Y')  # Update date format to dd-mm-yyyy
+    end_date = datetime.datetime.strptime(
+        end_date, '%d-%m-%Y')  # Update date format to dd-mm-yyyy
 
     total_crypto = amount / get_current_price(ticker_id, delay)
     total_investment = amount
-
 
     url = f"https://api.coingecko.com/api/v3/coins/{ticker_id}/market_chart/range?vs_currency=usd&from={int(start_date.timestamp())}&to={int(end_date.timestamp())}"
     print(f"\nRequesting URL: {url}\n")
@@ -58,7 +59,8 @@ def calculate_investments(start_date, end_date=None, step_percentage=1, amount=1
             for i in range(len(prices)):
                 timestamp = prices[i][0]
                 price_value = prices[i][1]
-                current_date = datetime.datetime.fromtimestamp(timestamp // 1000)  # Convert from milliseconds to seconds
+                current_date = datetime.datetime.fromtimestamp(
+                    timestamp // 1000)  # Convert from milliseconds to seconds
                 if current_date.date() > last_purchase_date.date():
                     # Check if the price has increased by the step percentage
                     if price_value >= (1 + step_percentage / 100) * total_investment / total_crypto:
@@ -67,20 +69,30 @@ def calculate_investments(start_date, end_date=None, step_percentage=1, amount=1
                         total_investment += amount
                         last_purchase_date = current_date
         else:
-            print(f"Unable to retrieve prices for {ticker} in the specified range.")
+            print(
+                f"Unable to retrieve prices for {ticker} in the specified range.")
     else:
-        print(f"Unable to retrieve prices for {ticker} in the specified range.")
+        print(
+            f"Unable to retrieve prices for {ticker} in the specified range.")
 
     return total_crypto, total_investment
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Calculate the result of step-up investments into a specific cryptocurrency.')
-    parser.add_argument('-s', '--start', help='Start date for the investment strategy in dd-mm-yyyy format.', required=True)
-    parser.add_argument('-e', '--end', help='End date for the investment strategy in dd-mm-yyyy format. If not provided, the current date will be used.')
-    parser.add_argument('-p', '--step_percentage', help='Step percentage for each purchase. For example, 1 for 1% step increase.', type=float, default=1)
-    parser.add_argument('-a', '--amount', help='Amount in USD for each purchase.', type=float, required=True)
-    parser.add_argument('-t', '--ticker', help='Ticker of the cryptocurrency. If not provided, BTC will be used.', default='BTC')
-    parser.add_argument('-d', '--delay', help='Delay in seconds between each API request.', type=int, default=DEFAULT_API_REQUEST_DELAY)
+    parser = argparse.ArgumentParser(
+        description='Calculate the result of step-up investments into a specific cryptocurrency.')
+    parser.add_argument(
+        '-s', '--start', help='Start date for the investment strategy in dd-mm-yyyy format.', required=True)
+    parser.add_argument(
+        '-e', '--end', help='End date for the investment strategy in dd-mm-yyyy format. If not provided, the current date will be used.')
+    parser.add_argument('-p', '--step_percentage',
+                        help='Step percentage for each purchase. For example, 1 for 1% step increase.', type=float, default=1)
+    parser.add_argument(
+        '-a', '--amount', help='Amount in USD for each purchase.', type=float, required=True)
+    parser.add_argument(
+        '-t', '--ticker', help='Ticker of the cryptocurrency. If not provided, BTC will be used.', default='BTC')
+    parser.add_argument('-d', '--delay', help='Delay in seconds between each API request.',
+                        type=int, default=DEFAULT_API_REQUEST_DELAY)
 
     args = parser.parse_args()
 
@@ -90,24 +102,29 @@ def main():
         print(f"Cryptocurrency with ticker '{args.ticker}' is not supported.")
         return
 
-    total_crypto, total_investment = calculate_investments(args.start, args.end, args.step_percentage, args.amount, args.ticker, ticker_id, args.delay)
+    total_crypto, total_investment = calculate_investments(
+        args.start, args.end, args.step_percentage, args.amount, args.ticker, ticker_id, args.delay)
     current_price = get_current_price(ticker_id, args.delay)
     price_of_investments = total_crypto * current_price if current_price else None
     delta_percent = ((price_of_investments - total_investment) / total_investment) * 100 if price_of_investments else None
 
     print(f"\nStarting parameters:")
     print(f"Start Date: {args.start}")
-    print(f"End Date: {args.end if args.end else 'Today (' + datetime.datetime.now().strftime('%d-%m-%Y') + ')'}")
+    print(
+        f"End Date: {args.end if args.end else 'Today (' + datetime.datetime.now().strftime('%d-%m-%Y') + ')'}")
     print(f"Step Percentage: {args.step_percentage}%")
     print(f"Investment Amount: ${args.amount}")
     print(f"Cryptocurrency Ticker: {args.ticker}\n")
     print(f"Total investment amount: ${total_investment:.2f}")
     print(f"Total amount of {args.ticker}: {total_crypto:.8f}")
     if price_of_investments:
-        print(f"Price of investments in fiat today: ${price_of_investments:.2f}")
-        print(f"Delta in percent between invested money and current price of investments: {delta_percent:.2f}%")
+        print(
+            f"Price of investments in fiat today: ${price_of_investments:.2f}")
+        print(
+            f"Delta in percent between invested money and current price of investments: {delta_percent:.2f}%")
     else:
         print("Failed to calculate the current value of investments.")
+
 
 if __name__ == "__main__":
     main()
